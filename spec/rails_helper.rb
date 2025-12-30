9aaffb725e9c6a755bf9cb13045d7120747c58a7
Kirk Wang <k3wang@gmail.com>
2025-12-29 23:22:57 -0800

    ⚙️ Clean up solr before test suite run
    This commit will make sure that the Solr index is cleared before the
    test suite runs, preventing any interference from leftover data.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -83,6 +83,8 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.allow_remote_database_url = true
     DatabaseCleaner.clean_with(:truncation)
+    Blacklight.default_index.connection.delete_by_query('*:*')
+    Blacklight.default_index.connection.commit
     User.group_service = TestHydraGroupService.new
   end
 end
