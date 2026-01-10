9f8dd58a531d23c51cfbfb4d07e30b549a704772
Kirk Wang <k3wang@gmail.com>
2026-01-09 20:36:18 -0800

    🧹 Have destroy and update take zips
    It was silly to just let create take a zip and add all that javascript
    to handle changing the accepted file types.  Now destroy and update can
    also take zips, and the javascript is removed.

--- app/components/skullrax/import_card_component.rb
+++ app/components/skullrax/import_card_component.rb
@@ -5,32 +5,5 @@ module Skullrax
     def import_path
       helpers.skullrax.imports_path
     end
-
-    def file_accept_script # rubocop:disable Metrics/MethodLength
-      content_tag(:script) do
-        <<~JS.html_safe
-          document.addEventListener('DOMContentLoaded', function() {
-            const actionRadios = document.querySelectorAll('input[name="import[action]"]');
-            const fileInput = document.querySelector('input[name="import[file]"]');
-
-            function updateFileAccept() {
-              const selectedAction = document.querySelector('input[name="import[action]"]:checked').value;
-
-              if (selectedAction === 'create') {
-                fileInput.setAttribute('accept', '.csv,.zip');
-              } else {
-                fileInput.setAttribute('accept', '.csv');
-              }
-            }
-
-            actionRadios.forEach(radio => {
-              radio.addEventListener('change', updateFileAccept);
-            });
-
-            updateFileAccept();
-          });
-        JS
-      end
-    end
   end
 end
