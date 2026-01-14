1014ad1d2c3e1cdc92835fe3ff80fa263aa6a5af
Kirk Wang <k3wang@gmail.com>
2026-01-14 08:10:04 -0800

    🐛 Fix bug with form best nested
    Forms were being nested and causing issues when submitting to the server
    where only the resources param was getting in.  This fix will now let
    the backend receive all the params.

--- app/components/skullrax/batch_create/card_component.rb
+++ app/components/skullrax/batch_create/card_component.rb
@@ -15,16 +15,6 @@ module Skullrax
         )
       end
 
-      def all_resource_types
-        [collection_value] + work_types + [file_set_value]
-      end
-
-      def form_for_resource_type(resource_type)
-        klass = Wings::ModelRegistry.reverse_lookup(resource_type.constantize) || resource_type.constantize
-        resource = klass.new
-        Hyrax::Forms::ResourceForm.for(resource:)
-      end
-
       private
 
       def work_types
--- app/components/skullrax/batch_create/templates_component.rb
+++ app/components/skullrax/batch_create/templates_component.rb
@@ -2,6 +2,20 @@
 
 module Skullrax
   module BatchCreate
-    class TemplatesComponent < Skullrax::BatchCreate::CardComponent; end
+    class TemplatesComponent < Skullrax::BatchCreate::CardComponent
+      def all_resource_types
+        [collection_value] + work_types + [file_set_value]
+      end
+
+      def form_for_resource_type(resource_type)
+        klass = Wings::ModelRegistry.reverse_lookup(resource_type.constantize) || resource_type.constantize
+        resource = klass.new
+        Hyrax::Forms::ResourceForm.for(resource:)
+      end
+
+      def form_builder(form)
+        SimpleForm::FormBuilder.new(form.model_name.param_key, form, self, {})
+      end
+    end
   end
 end
